0b199a8a6329e21a1840ce4a2bf5501b9bc6df00
Ahmadjon Safarov <ahmadjonsafarov1112@gmail.com>
2022-12-21 23:02:33 +0500

    WillPopScope

--- lib/page/add_todo_page.dart
+++ lib/page/add_todo_page.dart
@@ -23,60 +23,74 @@ class _AState extends State<A> {
   }
   @override
   Widget build(BuildContext context) {
-    return OnUnFocusTap(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Add Todo",style:  Style.TextStyleBold(),),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextFormField(
-              maxLines: 2,
-              maxLength: 40,
-              controller: textEditingController,
-              onChanged: (value){
-                if(value.isEmpty){
-                  isEmpty=false;
-                }else{
-                  isEmpty=true;
-                }
-                setState(() {});
-              },
-              decoration: InputDecoration(
-                labelText: "Text",
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+    return WillPopScope(
+      onWillPop: () {
+        showDialog(context: context, builder: (context){
+          return AlertDialog(
+            title: Text("chiqishni xoxlaysizmi"),
+            actions: [
+              ElevatedButton(onPressed: (){Navigator.pop(context);Navigator.pop(context);}, child: Text("Ha")),
+              ElevatedButton(onPressed: (){Navigator.pop(context);}, child: Text("Yoq")),
+            ],
+          );
+        });
+        return Future.value(false);
+      },
+      child: OnUnFocusTap(
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Add Todo",style:  Style.TextStyleBold(),),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextFormField(
+                maxLines: 2,
+                maxLength: 40,
+                controller: textEditingController,
+                onChanged: (value){
+                  if(value.isEmpty){
+                    isEmpty=false;
+                  }else{
+                    isEmpty=true;
+                  }
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  labelText: "Text",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+                ),
               ),
             ),
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: Container(
-          margin: EdgeInsets.symmetric(horizontal: 24),
-          width: double.infinity,
-          height: 60,
-          decoration: BoxDecoration(
-            gradient: isEmpty? Style.HomeGgradient:Style.Ggradient,
-            borderRadius: BorderRadius.circular(100),
-          ),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(100),
-            onTap: () async {
-              if(textEditingController.text.isNotEmpty){
-                LocalStore.setTodo(TodoModel(title: textEditingController.text));
-                // List<String> list=await LocalStore.getTodo();
-                // list.add(textEditingController.text);
-                // LocalStore.setList(list);
-                Navigator.pushAndRemoveUntil(context,
-                    MaterialPageRoute(builder: (_)=>HomePage()),
-                        (route) => false);
-              }
-            },
-            child: Center(
-              child: Text("Add",style:Style.TextStyleNormal,),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: Container(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              gradient: isEmpty? Style.HomeGgradient:Style.Ggradient,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(100),
+              onTap: () async {
+                if(textEditingController.text.isNotEmpty){
+                  LocalStore.setTodo(TodoModel(title: textEditingController.text));
+                  // List<String> list=await LocalStore.getTodo();
+                  // list.add(textEditingController.text);
+                  // LocalStore.setList(list);
+                  Navigator.pushAndRemoveUntil(context,
+                      MaterialPageRoute(builder: (_)=>HomePage()),
+                          (route) => false);
+                }
+              },
+              child: Center(
+                child: Text("Add",style:Style.TextStyleNormal,),
+              ),
             ),
           ),
         ),
--- lib/page/edit_todo.dart
+++ lib/page/edit_todo.dart
@@ -32,55 +32,69 @@ class _EditTodoPageState extends State<EditTodoPage> {
   }
   @override
   Widget build(BuildContext context) {
-    return OnUnFocusTap(
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Edit Todo",style:  Style.TextStyleBold(),),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextFormField(
-              autofocus: true,
-              maxLength: 35,
-              controller: textEditingController,
-              onChanged: (value){
-                if(value.isEmpty || oldTitle==value){
-                  isEmpty=false;
-                }else{
-                  isEmpty=true;
-                }
-                setState(() {});
-              },
-              decoration: InputDecoration(
-                  labelText: "Text",
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+    return WillPopScope(
+      onWillPop: () {
+        showDialog(context: context, builder: (context){
+          return AlertDialog(
+            title: Text("chiqishni xoxlaysizmi"),
+            actions: [
+              ElevatedButton(onPressed: (){Navigator.pop(context);Navigator.pop(context);}, child: Text("Ha")),
+              ElevatedButton(onPressed: (){Navigator.pop(context);}, child: Text("Yoq")),
+            ],
+          );
+        });
+        return Future.value(false);
+      },
+      child: OnUnFocusTap(
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Edit Todo",style:  Style.TextStyleBold(),),
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextFormField(
+                autofocus: true,
+                maxLength: 35,
+                controller: textEditingController,
+                onChanged: (value){
+                  if(value.isEmpty || oldTitle==value){
+                    isEmpty=false;
+                  }else{
+                    isEmpty=true;
+                  }
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                    labelText: "Text",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                    enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
+                ),
               ),
             ),
           ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: Container(
-          margin: EdgeInsets.symmetric(horizontal: 24),
-          width: double.infinity,
-          height: 60,
-          decoration: BoxDecoration(
-            gradient: isEmpty? Style.HomeGgradient:Style.Ggradient,
-            borderRadius: BorderRadius.circular(100),
-          ),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(100),
-            onTap: () async {
-              if(isEmpty){
-                LocalStore.setbool(TodoModel(title: textEditingController.text,isDone: widget.todomodel.isDone ), widget.index);
-                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomePage()), (route) => false);
-              }
-            },
-            child: Center(
-              child: Text("Edit",style:Style.TextStyleNormal,),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: Container(
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              gradient: isEmpty? Style.HomeGgradient:Style.Ggradient,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(100),
+              onTap: () async {
+                if(isEmpty){
+                  LocalStore.setbool(TodoModel(title: textEditingController.text,isDone: widget.todomodel.isDone ), widget.index);
+                  Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomePage()), (route) => false);
+                }
+              },
+              child: Center(
+                child: Text("Edit",style:Style.TextStyleNormal,),
+              ),
             ),
           ),
         ),
